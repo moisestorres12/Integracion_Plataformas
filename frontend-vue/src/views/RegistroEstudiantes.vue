b6d75639683aee809f6d313db11659958dd9a0c0
Moises Torres <moisestorres0121@gmail.com>
2022-06-01 15:27:22 -0600

    busqueda y ordenamiento

--- frontend-vue/src/views/RegistroEstudiantes.vue
+++ frontend-vue/src/views/RegistroEstudiantes.vue
@@ -6,7 +6,8 @@
         <br><br>
         <Toolbar class="mb-4">
             <template #start>
-                <Button label="Nuevo Estudiante" icon="pi pi-plus" class="mr-2" @click="openModal"/>
+            <Button label="Nuevo Estudiante" icon="pi pi-plus" class="mr-2" @click="openModal"/>
+             <input @input="searchtable" type="Search"  placeholder="Buscar..." class="form-control" input>
             </template>
 
             <template #end>
@@ -14,11 +15,20 @@
         </Toolbar>
         <!-- cuerpo del data table -->
          
-<DataTable :value="estudiantes">
-<Column v-for="col of columns" :field="col.field" :header="col.header" :key="col.field"></Column>
-</DataTable>
-       
 
+<DataTable :value="estudiantes" sortMode="multiple" :paginator="true" :rows="10" :first="firstRecordIndex">
+    <Column field="id" header="Id" :sortable="true"></Column>
+    <Column field="nombre" header="Nombre" :sortable="true"></Column>
+    <Column field="apellidos" header="Apellidos" :sortable="true"></Column>
+    <Column field="grupo" header="Grupo" :sortable="true"></Column>
+    <Column field="correo" header="Correo" :sortable="true"></Column>
+    <Column field="direccion.ciudad" header="Domicilio" :sortable="true"></Column>
+
+</DataTable>
+  
+
+
+       
 
     
         <!-- fin del cuerpo del data table -->
@@ -47,16 +57,49 @@ export default {
     }, 
     data(){
         return {
+            originaldata:[],
             displayModal: false,
             messages: [],
             columns:null,
-            estudiantes:null
+            estudiantes:null,
+
+            dialog: false,
+      dialogDelete: false,
+      headers: [
+        {
+          text: 'Id',
+          align: 'start',
+          sortable: false,
+          value: 'id',
+        },
+        { text: 'Nombre', value: 'nombre' },
+        { text: 'Apellido', value: 'apellidos' },
+        { text: 'Grupo', value: 'grupo' },
+        { text: 'Correo', value: 'correo' },
+        { text: 'Domicilio', value: 'direccion.ciudad', sortable: false },
+      ],
+      estudiantess: [],
+      editedIndex: -1,
+      editedItem: {
+        nombre: '',
+        apellidos: "",
+        grupo: "",
+        correo: "",
+        protein: 0,
+      },
+      defaultItem: {
+        name: '',
+        calories: 0,
+        fat: 0,
+        carbs: 0,
+        protein: 0,
+      },
         }
     },
     InformacionEstudiantes:null,
     created(){
         this.InformacionEstudiantes = new InformacionEstudiantes();
-        this.columns = [
+       /* this.columns = [
             //el field es el elemento dentro del json, o sea, la variable
             //el header es lo que se muestra en la pantalla
     {field:"id", header:"ID"},
@@ -65,15 +108,19 @@ export default {
     {field:"grupo", header:"Grupo"},
     {field:"correo",header:"Correo"},
     //aqui lleva un punto direccion, por que dentro de ella hay ciudad y nacionaliodad, y solo ocuparemos ciudad
-    {field:"direccion.ciudad",header:"Dirreccion"}
-        ]
+    {field:"direccion.ciudad",header:"Dirreccion"},
+    {field:"", header:"Opciones"}
+
+        ]*/
 
     },
 
     mounted(){
         //como estaba nulo le asignamos el listado de estudiantes segun la api
         this.InformacionEstudiantes.getStudents().then(data => this.estudiantes = data);
-    },
+        this.originaldata = this.data;
+        
+        },
     computed:{
         bindings(){
             return {
@@ -94,6 +141,23 @@ export default {
         saveInfo(){
             this.displayModal = false
             this.$toast.add({severity: 'success', summary: 'Agregado con Exito', detail: 'El estudiante ha sido agregado con exito.', life: 3000});
+
+        },
+        searchtable(){
+            var event =  event.target.value;
+            var response = [];
+            if(event != ""){
+                for(data of this.data){
+                    for(value of object.values(data)){
+                        if(value.indexof(event>=0 )){
+                        response.push(data);
+                        }
+                    }
+                }
+                this.data = response;
+            } else{
+                this.data = this.originaldata; 
+            }
 
         }
     }
